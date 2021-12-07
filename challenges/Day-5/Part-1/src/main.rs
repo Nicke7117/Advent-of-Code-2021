7b63e6255d8915c057021b6ea857814995f6efc1
= <nikolas.syverod@gmail.com>
2021-12-07 22:33:39 +0200

    Complete day 5

---
+++ challenges/Day-5/Part-1/src/main.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string("input.txt").expect("Failed to read input.txt");
+    let mut coordinates_hash = HashMap::new();
+    for line in data.lines() {
+        let line_coordinates = line.split(" -> ").collect::<Vec<&str>>();
+        let parsed_coordinates = line_coordinates
+            .iter()
+            .map(|coordinate| {
+                coordinate
+                    .split(",")
+                    .map(|x| x.parse::<u16>().unwrap())
+                    .collect::<Vec<u16>>()
+            })
+            .collect::<Vec<Vec<u16>>>();
+
+        if parsed_coordinates[0][0] == parsed_coordinates[1][0] {
+            let mut start: u16 = parsed_coordinates[0][1];
+            let mut end: u16 = parsed_coordinates[1][1];
+            if end < start {
+                start = parsed_coordinates[1][1];
+                end = parsed_coordinates[0][1];
+            }
+            for y in start..=end {
+                *coordinates_hash
+                    .entry((parsed_coordinates[0][0], y))
+                    .or_insert(0) += 1;
+            }
+        } else if parsed_coordinates[0][1] == parsed_coordinates[1][1] {
+            let mut start: u16 = parsed_coordinates[0][0];
+            let mut end: u16 = parsed_coordinates[1][0];
+            if end < start {
+                start = parsed_coordinates[1][0];
+                end = parsed_coordinates[0][0];
+            }
+            for x in start..=end {
+                *coordinates_hash
+                    .entry((x, parsed_coordinates[0][1]))
+                    .or_insert(0) += 1;
+            }
+        }
+    }
+    let mut amount: u16 = 0;
+    for val in coordinates_hash.values() {
+        if *val > 1 {
+            amount += 1;
+        }
+    }
+    println!("{}", amount);
+}
